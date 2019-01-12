7a3fb9fd54192e3f2f42411b6cd8a90dc2f36899
Keegan Tountas <keegantountas@yahoo.com>
2019-01-12 13:06:24 -0600

    obstacles/collision/health

--- Racer/GameElements.swift
+++ Racer/GameElements.swift
@@ -8,50 +8,30 @@
 
 import SpriteKit
 
-struct CollisionBitMask {
-    static let biker:UInt32 = 0x00
-    static let Obstacle:UInt32 = 0x01
-}
-
-enum ObstacleType:Int {
-    case Small = 0
-    case Medium = 1
-    case Large = 2
-}
-
-enum RowType:Int {
-    case SSmall = 0
-    case SMedium = 1
-    case SLarge = 2
-}
-
 extension GameScene{
     
     func bikerBuild () {
         
         biker.position = CGPoint(x: frame.size.width/2, y: frame.size.height/6)
-        biker.physicsBody = SKPhysicsBody(rectangleOf: biker.size)
+        biker.zPosition = 10
+        biker.physicsBody = SKPhysicsBody(texture: biker.texture!, size: biker.texture!.size())
         biker.physicsBody?.affectedByGravity = false
         biker.physicsBody?.isDynamic = true
-        biker.physicsBody?.friction = 0
-        biker.physicsBody?.restitution = 0
-        biker.physicsBody?.linearDamping = 0
         biker.name = "BIKER"
-        biker.physicsBody?.categoryBitMask = CollisionBitMask.biker
+        biker.physicsBody?.categoryBitMask = bikerCategory
+        biker.physicsBody?.contactTestBitMask = carCategory
         biker.physicsBody?.collisionBitMask = 0
-        
-        initialBikerPosition = biker.position
+        biker.physicsBody?.usesPreciseCollisionDetection = true
+    
     }
-    //new
+
     func moveBikerRight(){
+        
         biker.physicsBody?.velocity = CGVector(dx: 300, dy: 0)
         let bikerRight = SKTexture(imageNamed: "biker right")
         
-        //biker.anchorPoint = CGPoint(x: biker.position.x, y: (frame.size.height/6)-20)
         let rotateRight = SKAction.rotate(toAngle: -0.3, duration: 0.5)
         let steerRight = SKAction.animate(with: [bikerRight], timePerFrame: 0.1)
-        
-        //let rightMotion = SKAction.sequence([steerRight, rotateRight])
         
         biker.run(rotateRight)
         biker.run(steerRight)
@@ -62,11 +42,8 @@ extension GameScene{
         biker.physicsBody?.velocity = CGVector(dx: -300, dy: 0)
         let bikerLeft = SKTexture(imageNamed: "biker left")
         
-        //biker.anchorPoint = CGPoint(x: biker.position.x, y: (frame.size.height/6)-20)
         let rotateLeft = SKAction.rotate(toAngle: 0.3, duration: 0.5)
         let steerLeft = SKAction.animate(with: [bikerLeft], timePerFrame: 0.1)
-        
-        //let leftMotion = SKAction.sequence([steerLeft,rotateLeft])
         
         biker.run(rotateLeft)
         biker.run(steerLeft)
@@ -80,11 +57,8 @@ extension GameScene{
         
         let bikerCenter = SKTexture(imageNamed: "biker clean")
         
-        //biker.anchorPoint = CGPoint(x: biker.position.x, y: frame.size.height/6)
         let rotateCenter = SKAction.rotate(toAngle: 0, duration: 0.4)
         let steerCenter = SKAction.animate(with: [bikerCenter], timePerFrame: 0.1)
-        
-        //let centerMotion = SKAction.sequence([steerCenter,rotateCenter])
         
         biker.run(rotateCenter)
         biker.run(steerCenter)
@@ -105,42 +79,10 @@ extension GameScene{
         }
     }
     
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         bikerStop()
-    }
-    //old
-    
-    
-    func addObstacle (type:ObstacleType) -> SKSpriteNode {
-        let obstacle = SKSpriteNode(color:UIColor.white, size: CGSize(width:0, height: 30))
-        obstacle.name = "OBSTACLE"
-        obstacle.physicsBody?.isDynamic = true
-        
-        switch type {
-        case .Small:
-            obstacle.size.width = self.size.width * 0.2
-            break
-        case .Medium:
-            obstacle.size.width = self.size.width * 0.35
-            break
-        case .Large:
-            obstacle.size.width = self.size.width * 0.75
-            break
         }
-        obstacle.position = CGPoint(x:0,y:self.size.height + obstacle.size.height)
-        obstacle.physicsBody = SKPhysicsBody(rectangleOf: obstacle.size)
-        obstacle.physicsBody?.categoryBitMask = CollisionBitMask.Obstacle
-        obstacle.physicsBody?.collisionBitMask = 0
         
-        
-        return obstacle
     }
-    func addMovement (obstacle:SKSpriteNode) {
-        var actionArray = [SKAction]()
-        
-        actionArray.append(SKAction.move(to: CGPoint(x: obstacle.position.x, y: -obstacle.size.height), duration: TimeInterval(10.38)))
-        actionArray.append(SKAction.removeFromParent())
-        
-        obstacle.run(SKAction.sequence(actionArray))
-    }
-}
+
--- Racer/GameScene.swift
+++ Racer/GameScene.swift
@@ -10,110 +10,175 @@ import GameplayKit
 import UIKit
 import CoreMotion
 
-enum Direction: Int{
-    case None = 0
-    case Left = 1
-    case Right = 2
-}
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    
-    
     
     let brick1 = SKSpriteNode(imageNamed: "brick2png")
     let brick2 = SKSpriteNode(imageNamed: "brick2png")
     let biker = SKSpriteNode(imageNamed: "biker clean")
-    let motionManger = CMMotionManager()
+    //var puddlesArray = ["puddle 1","puddle 2","puddle 3","puddle 4","puddle 5","puddle 6","puddle 7"]
     
-    var xAcceleration:CGFloat = 0
-    var initialBikerPosition: CGPoint!
-    var obstacles = ["car"]
+    var currentHealth:Int = 450
+    var healthBar = SKSpriteNode()
+    var contactOnce = 0
+    
     var gameTimer:Timer!
+    var gameTimer2:Timer!
+    var gameTimer3:Timer!
+    var gameOver = false
     
+    let bikerCategory:UInt32 = 1 << 1
+    let carCategory:UInt32 = 1 << 2
+    let borderCategory:UInt32 = 1 << 3
+    let puddleCategory:UInt32 = 1 << 4
     
     override func didMove(to view: SKView) {
         
-        let boundary = SKSpriteNode()
-        boundary.size = self.frame.size
-        boundary.physicsBody?.isDynamic = true
-        boundary.physicsBody?.friction = 1.0
-        
-        
         self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         physicsWorld.contactDelegate = self
-        
         addChild(brick1)
         addChild(brick2)
         addChild(biker)
-        
+
         scrollingBackground1()
         scrollingBackground2()
         bikerBuild()
-        gameTimer = Timer.scheduledTimer(timeInterval: 0.75, target: self, selector: #selector(addObstacles), userInfo: nil, repeats: true)
         
-        motionManger.accelerometerUpdateInterval = 0.2
-        motionManger.startAccelerometerUpdates(to: OperationQueue.current!) { (data:CMAccelerometerData?, error:Error?) in
-            if let accelerometerData = data {
-                let acceleration = accelerometerData.acceleration
-                self.xAcceleration = CGFloat(acceleration.x) * 0.75 + self.xAcceleration * 0.25
-            }
-        }
-    }
+        healthBar.size = CGSize(width: currentHealth, height: 40)
+        healthBar.position = CGPoint(x: frame.size.width * 0.75, y: frame.size.height * 0.95)
+        healthBar.zPosition = 5
+        healthBar.color = .red
+        healthBar.name = "healthBar"
+        addChild(healthBar)
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(addObstacleCar), userInfo: nil, repeats: true)
+        gameTimer2 = Timer.scheduledTimer(timeInterval: 8, target: self, selector: #selector(addPuddle), userInfo: nil, repeats: true)
+        gameTimer3 = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(healthProgress), userInfo: nil, repeats: true)
+}
+
     
-    @objc func addObstacles() {
-        obstacles = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: obstacles) as! [String]
+    //cars start
+    @objc func addObstacleCar() {
+        print("obstaclecar")
+        let obstacleCar = SKSpriteNode(imageNamed: "car")
         
-        let obstacle = SKSpriteNode(imageNamed: obstacles[0])
+        let positionArrayX = [frame.size.width/8,frame.size.width * 0.375, frame.size.width * 0.625,frame.size.width * 0.875]
+        let nextPosition = positionArrayX[Int(arc4random_uniform(UInt32(positionArrayX.count)))]
         
-        let randomObstaclePosition = GKRandomDistribution(lowestValue: 0, highestValue: 1080)
-        let position = CGFloat(randomObstaclePosition.nextInt())
+        obstacleCar.position = CGPoint(x: nextPosition, y: frame.size.height * 1.5)
+        obstacleCar.zPosition = 2
+        obstacleCar.physicsBody = SKPhysicsBody(texture: obstacleCar.texture!, size: obstacleCar.texture!.size())
         
-        obstacle.position = CGPoint(x: position, y: self.frame.size.height + obstacle.size.height)
+        obstacleCar.physicsBody?.categoryBitMask = carCategory
+        obstacleCar.physicsBody?.contactTestBitMask = bikerCategory
+        obstacleCar.physicsBody?.collisionBitMask = 0
         
-        obstacle.physicsBody = SKPhysicsBody(rectangleOf: obstacle.size)
-        obstacle.physicsBody?.isDynamic = true
+        self.addChild(obstacleCar)
         
-        obstacle.physicsBody?.categoryBitMask = CollisionBitMask.Obstacle
-        obstacle.physicsBody?.collisionBitMask = CollisionBitMask.biker
-        
-        self.addChild(obstacle)
-        
-        let animationDuration:TimeInterval = 6
+        let animationDuration:TimeInterval = 20
         
         var actionArray = [SKAction]()
         
-        actionArray.append(SKAction.move(to: CGPoint(x: position, y: -obstacle.size.height), duration: animationDuration))
+        if nextPosition > frame.size.width/2 {
+            actionArray.append(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 0))
+        }
+        
+        actionArray.append(SKAction.moveBy(x: 0, y: -frame.size.height * 2, duration: animationDuration))
         actionArray.append(SKAction.removeFromParent())
         
-        obstacle.run(SKAction.sequence(actionArray))
+        obstacleCar.run(SKAction.sequence(actionArray))
     }
     
-    var lastUpdateTimeInterval = TimeInterval()
-    var lastYieldTimeInterval = TimeInterval()
+    //cars end
     
+    //puddles start
     
-    func updateWithTimeSinceLastUpdate (timeSinceLastUpdate:TimeInterval) {
-        lastYieldTimeInterval += timeSinceLastUpdate
-        if lastYieldTimeInterval > 2 {
-            lastYieldTimeInterval = 0
+    @objc func addPuddle() {
+        print("puddle")
+        var puddlesArray = ["puddle 1","puddle 2","puddle 3","puddle 4","puddle 5","puddle 6","puddle 7"]
+        
+        puddlesArray = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: puddlesArray) as! [String]
+
+        let puddle = SKSpriteNode(imageNamed: puddlesArray[0])
+        
+        let randomPuddlePosition = GKRandomDistribution(lowestValue: 100, highestValue:980)
+        let position = CGFloat(randomPuddlePosition.nextInt())
+        
+        puddle.position = CGPoint(x: position, y: self.frame.size.height * 1.5)
+        puddle.zPosition = 1
+        
+        puddle.physicsBody = SKPhysicsBody(texture:puddle.texture!, size: puddle.texture!.size())
+        puddle.physicsBody?.isDynamic = true
+        
+        puddle.physicsBody?.categoryBitMask = puddleCategory
+        puddle.physicsBody?.contactTestBitMask = bikerCategory
+        puddle.physicsBody?.collisionBitMask = 0
+        
+        self.addChild(puddle)
+        
+        let animationDuration:TimeInterval = 20
+        
+        var actionArray = [SKAction]()
+        
+        actionArray.append(SKAction.moveBy(x: 0, y: -frame.size.height * 2, duration: animationDuration))
+        actionArray.append(SKAction.removeFromParent())
+        
+        puddle.run(SKAction.sequence(actionArray))
+    }
+    
+    //puddles end
+    
+    //health begin
+    func healthAnimation() {
+        let removeHealth = SKAction.resize(toWidth: CGFloat(currentHealth), duration: 0.1)
+        healthBar.run(removeHealth)
+        if currentHealth == 0 {
+            currentHealth = 0
+            print("game over")
+            
         }
     }
-    
-    override func update(_ currentTime: CFTimeInterval) {
-        var timeSinceLastUpdate = currentTime - lastUpdateTimeInterval
-        lastUpdateTimeInterval = currentTime
-        
-        if timeSinceLastUpdate > 1 {
-            timeSinceLastUpdate = 1/60
-            lastUpdateTimeInterval = currentTime
-        }
-        
-        updateWithTimeSinceLastUpdate(timeSinceLastUpdate: timeSinceLastUpdate)
+    @objc func healthProgress() {
+        healthAnimation()
     }
+
     
+    //health end
     func didBegin(_ contact: SKPhysicsContact) {
-        if contact.bodyA.node?.name == "BIKER"{
-            print("HIT")
+        
+        let contactMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+        
+        switch contactMask {
+        case bikerCategory | carCategory:
+            
+            currentHealth -= 10
+            if currentHealth <= 0 {
+                currentHealth = 0
+                gameTimer3.invalidate()
+            }
+            if contactOnce == 1{
+                break
+            }
+            
+            
+        case bikerCategory | puddleCategory:
+
+            currentHealth -= 10
+            if currentHealth <= 0 {
+                currentHealth = 0
+                gameTimer3.invalidate()
+            }
+            if contactOnce == 1{
+                break
+            }
+            
+        default:
+            contactOnce = 0
         }
+}
+
+    override func update(_ currentTime: TimeInterval){
+        return
     }
+
 }
